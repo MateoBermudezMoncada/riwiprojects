490c86df7e2bd3017e550292136b15ce401bf004
Mateo Bermudez <mateobermudezmoncada@gmail.com>
2024-10-06 20:10:16 -0500

    initial commit

---
+++ src/main/java/com/riwi/riwi_projects/RiwiProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.riwi_projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiwiProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiwiProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/riwi_projects/RiwiProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.riwi_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwiProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
